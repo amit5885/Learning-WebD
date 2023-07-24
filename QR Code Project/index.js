af9eeffc56f97650784e1c2132fbd559eaf36f98
Amit Rana <95636836+amit5885@users.noreply.github.com>
2023-07-24 23:28:53 +0530

    Add files via upload

---
+++ QR Code Project/index.js
@@ -0,0 +1,32 @@
+// first install required node modules "npm i"
+import inquirer from "inquirer";
+import qr from "qr-image";
+import fs, { writeFile } from "fs";
+
+inquirer
+  .prompt([
+    {
+      /* Pass your questions in here */
+      message: "Type in your URL: ",
+      name: "URL",
+    },
+  ])
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+    const url = answers.URL;
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream("qr_image.png"));
+
+    //save input in URL.txt file
+    fs.writeFile("URL.txt", url, (err) => {
+      if (err) throw err;
+      console.log("The file has been saved!");
+    });
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
